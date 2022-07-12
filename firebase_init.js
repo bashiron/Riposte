095a0d84cc7cf32e570a48c9359aae4c6f00bf5b
Luciano Amoruso <luciano01amoruso@gmail.com>
2022-07-12 18:41:10 -0300

    package.json y firebase config

---
+++ firebase_init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwLG48qzunyCoUqcnMAwLCK0SHC3KtUog",
+  authDomain: "bashironthreadreader.firebaseapp.com",
+  projectId: "bashironthreadreader",
+  storageBucket: "bashironthreadreader.appspot.com",
+  messagingSenderId: "420784594649",
+  appId: "1:420784594649:web:a836837b3e9b08a555e8c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
