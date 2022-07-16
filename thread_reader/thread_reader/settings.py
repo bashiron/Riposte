a1a447cfb5e74a1120d00c027b4330042cd97db7
Luciano Amoruso <luciano01amoruso@gmail.com>
2022-07-16 14:01:20 -0300

    GET Tweet
    - Uso regex para extraer el ID del link
    - Hago un request a la API pidiendo los detalles de un tweet usando el ID
    - Instalo el debug-toolbar de Django

--- thread_reader/thread_reader/settings.py
+++ thread_reader/thread_reader/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -128,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 env = environ.Env()
 environ.Env.read_env()
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- thread_reader/threads/views.py
+++ thread_reader/threads/views.py
@@ -1,8 +1,11 @@
+from urllib import response
+from wsgiref import headers
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import requests, os, json
+import requests, os, json, re
 import environ
 from .forms import LinkForm
+from pdb import set_trace
 
 env = environ.Env()
 
@@ -10,7 +13,7 @@ def home(request):
     if request.method == 'POST':
         form = LinkForm(request.POST)
         if form.is_valid():
-            tw_id = extractId(form.cleaned_data)
+            tw_id = extractId(form.cleaned_data['tw_link'])
             return HttpResponseRedirect(f'thread/{tw_id}')
     else:
         form = LinkForm()
@@ -18,10 +21,10 @@ def home(request):
     return render(request, 'threads/home.html', {'form': form})
 
 def extractId(url):
-    #<extraer el id del url usando regex>
-    return 0
+    return re.search(r"\d{19}", url).group()
 
-def thread(request):
-    #<Traer thread de la API y cargarlo en el context>
-    context = {}
-    return render(request, 'threads/thread.html', context)
+def thread(request, id):
+    heads = {'Authorization': f'Bearer { env("BEARER_TOKEN") }'}
+    res = requests.get('https://api.twitter.com/2/tweets/', params={'ids': str(id)}, headers=heads)
+    # set_trace()
+    return render(request, 'threads/thread.html', {'response': res})
