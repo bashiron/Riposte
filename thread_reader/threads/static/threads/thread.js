bbbe7dd167312a608fc0223a1b98949b07230c0f
Luciano Amoruso <luciano01amoruso@gmail.com>
2022-08-09 21:33:49 -0300

    Tweet Image Popup (parte 2)
    Cambio la estructura del html para que en vez de contener solo filas, el container tenga un div (nivel) que encapsule una fila y una "fila" de popups asociados a los tweets de la fila
    - Traigo de internet algo de html, js y css de un ejemplo de popups para probar
    - En el script cambio las funciones principales para reflejar la nueva estructura del html
    - Creo una funcion que inicializa unas variables para no tener que hacerlo en la consola de chrome cada vez que las necesite

--- thread_reader/threads/static/threads/thread.js
+++ thread_reader/threads/static/threads/thread.js
@@ -2,6 +2,8 @@ let media_url = undefined;
 let more_url = undefined;
 let open_url = undefined;
 
+let art, links;    //variables para debugear en consola
+
 function storeUrls(media, more, open) {
     media_url = media;
     more_url = more;
@@ -9,9 +11,11 @@ function storeUrls(media, more, open) {
 }
 
 function moreReplies(boton) {
-    const fila = boton.parent();
+    const nivel = boton.parent().parent();
+    const fila = nivel.children().eq(0) //la lista de tweets
+    const popups = nivel.children().eq(1) //la lista de popups
     const tok = boton.attr('data-token');
-    const twid = fila.attr('data-id');
+    const twid = nivel.attr('data-id');
     $.ajax(
         {
             type: 'GET',
@@ -22,13 +26,15 @@ function moreReplies(boton) {
             },
             success: function (res) {
                 const tweets = prepareTweets(res);
+                const pops = preparePopups(res);
                 if (res.token) {
                     tweets.push(boton.attr('data-token', res.token));   //actualizo el token y muevo el boton al final de la lista
                 } else {
                     boton.remove();
                 }
                 fila.append(tweets);
-                updateSupports(extractLevel(fila));
+                popups.append(pops);
+                updateSupports(extractLevel(nivel));
             }
         }
     );
@@ -36,7 +42,7 @@ function moreReplies(boton) {
 
 //genera el html y define los handlers a eventos
 function prepareTweets(res) {
-    const tweets = generateElements(res);
+    const tweets = generateTweets(res);
     tweets.forEach(t => setTweetHandlers(t));
     return tweets;
 }
@@ -59,15 +65,17 @@ function setClickHandler(tweet) {
 function setMouseHandlers(tweet) {
     tweet.mouseenter(function () {
         $(this).addClass('focused');
-        $(this).find('.images-popup img').addClass('show');
+        // css.sheet.insertRule(`.images-popup{opacity: 1; z-index: 1000; display: block}`, 0);
+        // $(this).find('.images-popup img').addClass('show');
     });
     tweet.mouseleave(function () {
         $(this).removeClass('focused');
-        $(this).find('.images-popup img').removeClass('show');
+        // css.sheet.deleteRule(0);
+        // $(this).find('.images-popup img').removeClass('show');
     });
 }
 
-function generateElements(res) {
+function generateTweets(res) {
     const elems = [];
     for (let i = 0; i < res.items.length; i++) {
         const support = $('<div/>', {
@@ -92,19 +100,10 @@ function generateElements(res) {
             'class': 'article-content',
             html: res.items[i].text
         });
-        const image = $('<img/>', {
-            src: 'https://pbs.twimg.com/media/FZc_1mfWQAAugSo?format=jpg&name=small',
-            width: '100',
-            height: '100'
-        });
-        const popup = $('<span/>', {
-            'class': 'images-popup',
-            html: image
-        });
         const articulo = $('<article/>', {
             'class': 'content-section',
             'data-id': res.items[i].id,
-            html: [metadata, parrafo_cont, popup, link]
+            html: [metadata, parrafo_cont, link]
         });
         elems.push(articulo);
     }
@@ -126,10 +125,34 @@ function setMetadataHandlers(meta) {
     );
 }
 
+function preparePopups(res) {
+    const pops = generatePopups(res);
+    return pops;
+}
+
+function generatePopups(res) {
+    const elems = [];
+    for (let i = 0; i < res.items.length; i++) {
+        const image = $('<img/>', {
+            src: 'https://pbs.twimg.com/media/FZc_1mfWQAAugSo?format=jpg&name=small',
+            width: '100',
+            height: '100'
+        });
+        const popup = $('<span/>', {
+            'class': 'images-popup',
+            html: res.items[i].name
+            // html: image
+        });
+        elems.push(popup);
+    }
+    return elems;
+}
+
 function openThread(reply) {
     const fila = reply.parent();
-    const container = fila.parent();
-    const lvl = extractLevel(fila);
+    const nivel = fila.parent();
+    const container = nivel.parent();
+    const lvl = extractLevel(nivel);
     const twid = reply.attr('data-id');
     reply.off('mouseleave');    //desactivo el handler de cuando saco el mouse
     reply.addClass('active');
@@ -138,9 +161,10 @@ function openThread(reply) {
     fila.children('article:not(.active)').off('click mouseenter mouseleave');    //desactivo el handler de click para todos los otros tweets
     fila.find('.load-more').off('click')
     const new_fila = $('<div/>', {
-        id: 'lv-' + (lvl+1),
-        'class': 'fila',
-        'data-id': twid
+        'class': 'fila'
+    });
+    const new_popups = $('<div/>', {
+        'class': 'popups'
     });
     $.ajax(
         {
@@ -151,6 +175,7 @@ function openThread(reply) {
             },
             success: function (res) {
                 const tweets = prepareTweets(res);
+                const pops = preparePopups(res);
                 if (res.token) {
                     const btn = $('<a/>', {
                         'class': 'load-more btn btn-primary btn-lg',
@@ -166,10 +191,17 @@ function openThread(reply) {
                     tweets.push(btn);
                 }
                 new_fila.append(tweets);
-                $('#lv-' + (lvl+1)).replaceWith(new_fila);
-                container.append($('<div/>', {  //dejo una fila vacia para el siguiente openThread
+                new_popups.append(pops);
+                const new_nivel = $('<div/>', {
+                    id: 'lv-' + (lvl+1),
+                    'class': 'nivel',
+                    'data-id': twid,
+                    html: [new_fila, new_popups]
+                });
+                $('#lv-' + (lvl+1)).replaceWith(new_nivel);
+                container.append($('<div/>', {  //dejo un nivel vacio para el siguiente openThread
                     id: 'lv-' + (lvl+2),
-                    'class': 'fila'
+                    'class': 'nivel'
                 }));
                 updateSupports(lvl+1);
             }
@@ -177,15 +209,16 @@ function openThread(reply) {
     );
 }
 
-//tomo el numero de la fila
-function extractLevel(fila) {
-    return parseInt(fila.attr('id').substr(3));
+//tomo el numero del nivel
+function extractLevel(nivel) {
+    return parseInt(nivel.attr('id').substr(3));
 }
 
 function closeThread(reply) {
     const fila = reply.parent();
-    const container = fila.parent();
-    const lvl = extractLevel(fila);
+    const nivel = fila.parent();
+    const container = nivel.parent();
+    const lvl = extractLevel(nivel);
     reply.mouseleave(function () {
         $(this).removeClass('focused');
     });
@@ -199,18 +232,52 @@ function closeThread(reply) {
     fila.find('.load-more').click(function () {
         moreReplies($(this));
     });
-    container.children().slice(lvl+1).remove();   //borro las filas que vienen delante
+    container.children().slice(lvl+1).remove();   //borro los niveles que vienen delante
     container.append($('<div/>', {
         id: 'lv-' + (lvl+1),
-        'class': 'fila'
+        'class': 'nivel'
     }));
     reply.removeClass('active');
 }
 
 function updateSupports(lvl) {
-    const sups = $('#lv-' + lvl).children('article').children('.article-metadata').children('.support').slice(-10); //los ultimos 10 ya que como maximo, y la mayoria de las veces, cargo 10 tweets
+    const tweets = $('#lv-' + lvl).children().eq(0).children('article');
+    const sups = tweets.children('.article-metadata').children('.support').slice(-10); //los ultimos 10 ya que como maximo, y la mayoria de las veces, cargo 10 tweets
     sups.each(function () {
         const txt = $(this).parent().find('.user-name');
         $(this).css('height', txt.height());
     });
 }
+
+//-------------variables para consola----------------
+
+function consoleInit() {
+    console.log('Variables inicializadas');
+    art = $('#lvl-1').children().eq(2);
+    links = $('.link');
+}
+
+
+
+//                              PRUEBA POPUPS                              
+
+
+let index, index2;
+
+ function activarSpans() {
+     $('.tip span').hover(
+         function () {
+             var rect = $(this)[0].getBoundingClientRect();
+             var top = rect.top;
+             var bottom = rect.bottom;
+             var left = rect.right;
+          
+             index = css.sheet.insertRule(`.tip span::before{left:${left - 50}px;top:${top}px}`, 0);
+             index2 = css.sheet.insertRule(`.tip span::after{left:${left - 50}px;top:${top + 20}px}`, 0);
+             },
+         function () {
+             css.sheet.deleteRule(0);
+             css.sheet.deleteRule(0);
+         }
+     );
+ }
