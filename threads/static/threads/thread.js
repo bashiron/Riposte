836d293f6df2a48d24a8a37d747e214040c63253
Luciano Amoruso <luciano01amoruso@gmail.com>
2022-10-11 22:33:09 -0300

    Base Media (parte 2)
    Implemento una version muy modificada de unos slides de imagenes de un tutorial de W3Schools (https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow).
    
    Template (tweet)
    - Al <article> le quito la clase de Bootstrap porque voy a definir el tamaño desde CSS
    - Cambio el 'base-content' para que ahora el texto este contenido en un div, las imagenes ahora funcionan como un slideshow manual
    
    Template (base)
    - Envuelvo el navbar en un div que le da un espacio alrededor para que los contenidos de la pagina no queden detras del navbar
    
    Script
    - Traigo las funciones del ejemplo de slideshow
    
    CSS
    - Defino un tamaño minimo para el tweet base
    - Traigo el codigo del ejemplo de slideshow y hago muchos cambios
    - El 'base-media' tiene un tamaño fijo el cual es heredado por el 'my-slide' y la imagen en si
    - Usando 'object-fit' y 'object-position' se ajusta la imagen al estilo preview de twitter (ocupando todo su contenedor)

--- threads/static/threads/thread.js
+++ threads/static/threads/thread.js
@@ -378,3 +378,36 @@ function consoleInit() {
     art = $('#lv-1').children().eq(0).children().eq(2);
     links = $('.link');
 }
+
+
+
+
+
+
+
+
+
+//----------SLIDES--------------
+
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";
+}
